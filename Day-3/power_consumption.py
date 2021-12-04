cbddbc12457aaa7e6d0efbf9fcc56750eda36e06
jramirez857 <jramirez857@users.noreply.github.com>
2021-12-04 12:45:59 -0500

    Day 3: Part One

---
+++ Day-3/power_consumption.py
@@ -0,0 +1,15 @@
+import numpy as np
+bin_nums = np.array([list(i) for i in open('input.txt','r').read().strip().split('\n')], dtype='int')
+def binary_to_decimal(binary: list):
+# Raises each binary digit to the power of the index and adds them together
+# to get the decimal value.
+  return sum(n * 2**i for i,n in enumerate(binary[::-1]))
+
+# Calculates the gamma rate using the sum of the binary digits in each column
+# of the binary array. If the sum is greater than than half the size of the array, 
+# add 1 to the gamma rate. Otherwise, add 0. Do this for every column in the array
+# by checking the size of the first array.
+gamma_rate = [1 if sum(bin_nums[:, i]) > len(bin_nums[:, i])/2 else 0 for i in range(len(bin_nums[0,:]))]
+epsilon_rate = [2**i % 2 for i in gamma_rate]
+print('Part 1 result is:', binary_to_decimal(gamma_rate)*binary_to_decimal(epsilon_rate))
+
